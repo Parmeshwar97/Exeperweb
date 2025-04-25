e8a05e32edbef67a527de03949811b8f64a280a9
Parmeshwar Patil <parmeshwarpatil9763@gmail.com>
2025-04-25 22:45:47 +0530

    refactor in portfolio

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import { projects } from "../../data/Projects";
+import { projects } from "../../data/projectsData.js";
 import PortfolioCard from "../PortfolioCard";
 import GetStarted from "./services/GetStarted";
 
 const Portfolio = () => {
   return (
-    <div style={{fontFamily:'Segoe UI'}}>
+    <div style={{ fontFamily: "Segoe UI" }}>
       <div className="md:pt-32 pt-22 flex px-5 md:px-0 flex-col items-center">
         <p className="text-lg font-semibold tracking-wide">Our Portfolio</p>
-        <h2 className="font-bold text-3xl md:text-4xl my-2">Our Completed Projects</h2>
+        <h2 className="font-bold text-3xl md:text-4xl my-2">
+          Our Completed Projects
+        </h2>
         <p className="md:w-[600px] mt-1 text-center">
           There are many variations of passages of Lorem Ipsum available but the
           majority have suffered alteration in some form.
